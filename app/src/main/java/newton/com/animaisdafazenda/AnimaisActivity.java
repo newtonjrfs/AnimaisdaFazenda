8ce5ff5977d4c78fc3c357b0cfc9420e2165c0e4
Newton <newtoljunior@hotmail.com>
2018-01-15 13:38:25 -0200

    criacao do banco de dados e insercao de informacoes na tabela principal

--- app/src/main/java/newton/com/animaisdafazenda/AnimaisActivity.java
+++ app/src/main/java/newton/com/animaisdafazenda/AnimaisActivity.java
@@ -1,13 +1,18 @@
 package newton.com.animaisdafazenda;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class AnimaisActivity extends Activity {
     final int MILISEGUNDOS = 2500;
+    SQLiteDatabase bancoDeDados;
 
 
     @Override
@@ -15,6 +20,27 @@ public class AnimaisActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animais);
 
+        //criando o banco de dados
+        bancoDeDados = openOrCreateDatabase("Banco", Context.MODE_PRIVATE,null);
+        bancoDeDados.execSQL("CREATE TABLE IF NOT EXISTS tbl_animais (Codigo INT NOT NULL,Nome VARCHAR, Especie VARCHAR, Porte VARCHAR)");
+
+
+        //inserir os dados
+        /*
+        bancoDeDados.execSQL("INSERT INTO tbl_animais VALUES ('1','Galinha','Aves','Pequeno');");
+        bancoDeDados.execSQL("INSERT INTO tbl_animais VALUES ('2','Porco','Suino','Medio');");
+        bancoDeDados.execSQL("INSERT INTO tbl_animais VALUES ('3','Boi','Bovino','Grande');");
+        bancoDeDados.execSQL("INSERT INTO tbl_animais VALUES ('4','Vaca','Bovino','Grande');");
+        bancoDeDados.execSQL("INSERT INTO tbl_animais VALUES ('5','Bezerra','Bovino','Medio');");
+        bancoDeDados.execSQL("INSERT INTO tbl_animais VALUES ('6','Patos','Aves','Pequeno');");
+        bancoDeDados.execSQL("INSERT INTO tbl_animais VALUES ('7','Peru','Aves','Pequeno');");
+        bancoDeDados.execSQL("INSERT INTO tbl_animais VALUES ('8','Egua','Equino','Grande');");
+        bancoDeDados.execSQL("INSERT INTO tbl_animais VALUES ('9','Cavalo','Equino','Grande');");
+        bancoDeDados.execSQL("INSERT INTO tbl_animais VALUES ('10','Peixe','Peixes','Medio');");
+        */
+
+
+        //contador de tempo pra ir pra tela do main
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run() {
